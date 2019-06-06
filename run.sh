9fe46ce27ed09f1a7fe33a8c82fbaadb69383c42
waynamigo <waynamigo@gmail.com>
2019-06-06 09:04:56 +0800

    fixpath

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ else
 fi
 done
 
-for java_lib in $(ls $path/plugin/libs|grep -v lanterna);do
+for java_lib in $(ls $path/libs|grep -v lanterna);do
 if [[ libs != "" && libs != "lanterna.jar" ]];then
-    libs=$libs:$path/plugin/libs/$java_lib
+    libs=$libs:$path/libs/$java_lib
 else
-    libs=$path/plugin/libs/$java_lib
+    libs=$path/libs/$java_lib
 fi
 done
 
